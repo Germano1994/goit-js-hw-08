bbb186821c4080c0af9486283ce2a28cc443eb4c
Germano1994 <125311105+Germano1994@users.noreply.github.com>
2023-07-01 16:02:05 +0300

    added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,5 +4,36 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+
+const collection = document.querySelector('.gallery');
+
+const listItems = galleryItems.map(({ preview, original, description }) =>
+  `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+).join('');
+
+collection.insertAdjacentHTML('afterbegin', listItems);
+
+collection.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const largeImageUrl = event.target.dataset.source;
+  const lightbox = SimpleLightbox.create(`<img src="${largeImageUrl}" />`).show();
+
+});
   
