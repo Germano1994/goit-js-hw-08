565524415f8d94ef95c7f68dc1c1351b5dca1b90
Germano1994 <125311105+Germano1994@users.noreply.github.com>
2023-07-01 15:21:41 +0300

    added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }
+
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+    if (currentTime) {
+      player.setCurrentTime(parseFloat(currentTime));
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+form.addEventListener('input', throttle(saveFormState, 500));
+
+
+function saveFormState() {
+    const formState = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+  
+    localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+  }
+
+  
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const savedFormState = localStorage.getItem('feedback-form-state');
+  
+    if (savedFormState) {
+      const parsedFormState = JSON.parse(savedFormState);
+      emailInput.value = parsedFormState.email;
+      messageInput.value = parsedFormState.message;
+    }
+  });
+
+  
+
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+  
+    const formState = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+  
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+  
+    console.log(formState);
+  });
+  
