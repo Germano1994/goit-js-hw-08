8b0dd9915d6ea32cf4c505e5ce59a0d75cda4414
Germano1994 <125311105+Germano1994@users.noreply.github.com>
2023-07-02 20:08:33 +0300

    added files

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,43 +6,50 @@ const messageInput = form.querySelector('textarea[name="message"]');
 
 form.addEventListener('input', throttle(saveFormState, 500));
 
-
 function saveFormState() {
-    const formState = {
-      email: emailInput.value,
-      message: messageInput.value,
-    };
-  
-    localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const savedFormState = localStorage.getItem('feedback-form-state');
+
+  if (savedFormState) {
+    const parsedFormState = JSON.parse(savedFormState);
+    emailInput.value = parsedFormState.email;
+    messageInput.value = parsedFormState.message;
   }
+});
 
-  
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-  window.addEventListener('DOMContentLoaded', () => {
-    const savedFormState = localStorage.getItem('feedback-form-state');
-  
-    if (savedFormState) {
-      const parsedFormState = JSON.parse(savedFormState);
-      emailInput.value = parsedFormState.email;
-      messageInput.value = parsedFormState.message;
-    }
-  });
+  const emailValue = emailInput.value.trim();
+  const messageValue = messageInput.value.trim();
 
-  
-
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-  
+  if (emailValue !== '' && messageValue !== '') {
     const formState = {
-      email: emailInput.value,
-      message: messageInput.value,
+      email: emailValue,
+      message: messageValue,
     };
-  
+
     localStorage.removeItem('feedback-form-state');
     emailInput.value = '';
     messageInput.value = '';
-  
+
     console.log(formState);
-  });
-  
+  } else if (emailValue === '' && messageValue === '') {
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+  } else {
+    alert('Please fill in both fields.');
+  }
+});
+
+
+
